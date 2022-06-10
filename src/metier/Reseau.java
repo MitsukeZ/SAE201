667842e278957d86bf1a2ba328330ba548f8f7c7
mitsus <florianbizet2@gmail.com>
2022-06-10 11:03:04 +0200

    début réseau

--- src/metier/Reseau.java
+++ src/metier/Reseau.java
@@ -7,5 +7,33 @@ import java.util.ArrayList;
 public class Reseau 
 {
     private List<Cuve> lstCuve;
-    private List<Tube> lstTube;    
+    private List<Tube> lstTube;
+    
+    public  Reseau()
+    {
+        this.lstCuve = new ArrayList<Cuve>();
+        this.lstTube = new ArrayList<Tube>();
+    }
+
+    public boolean creerCuve(int capacite, int posX, int posY, String posInfo) {
+        Cuve cuveACreer = Cuve.fabrique(capacite, posX, posY, posInfo);
+
+        if (cuveACreer == null) {return false;}
+
+        this.lstCuve.add(cuveACreer);
+        return true;
+    }
+
+    public boolean creerTube(Cuve cv1, Cuve cv2, int epaisseur) {
+        Tube tubeACreer;
+        
+        
+        
+        tubeACreer = Tube.creerTube(cv1, cv2, epaisseur);
+
+        if (tubeACreer == null) {return false;}
+
+        this.lstTube.add(tubeACreer);
+        return true;
+    }
 }
