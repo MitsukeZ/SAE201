54a0386764d05cf541176cd80acfea94e420d352
Alexandra Desplan <alexandradesplan@gmail.com>
2022-06-10 11:04:15 +0200

    Controleur

--- src/Controleur.java
+++ src/Controleur.java
@@ -3,14 +3,18 @@ import iut.algo.Clavier;
 public class Controleur
 {
     private Reseau           metier;
-    private Generateur       generateur;
     private FrameFormulaire  ihm;
 
     public Controleur()
     {
         this.metier     = new Reseau(this);
-        this.generateur = new Generateur(this);
         this.ihm        = new FrameFormulaire(this);
+    }
+
+    public boolean creerCuve(int capacite, int posX, int posY, String posInfo) 
+    {
+        
+
     }
     
     public static void main(String[] args) 
