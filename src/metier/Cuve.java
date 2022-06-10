db4a6e379a410c689dcee47e90b4ac301db4d193
mitsus <florianbizet2@gmail.com>
2022-06-10 09:40:17 +0200

    Classe Cuve

--- src/metier/Cuve.java
+++ src/metier/Cuve.java
@@ -1,3 +1,4 @@
+package metier;
 public class Cuve
 {
 	//caractère auto-incrémenté allant de A à Z
---
+++ src/metier/Reseau.java
@@ -0,0 +1,11 @@
+package metier;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Reseau 
+{
+    private List<Cuve> lstCuve;
+    private List<Tube> lstTube;    
+}
