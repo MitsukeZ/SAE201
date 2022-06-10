c4378c2e6346aff694eb8b1934bb211d9cccfa21
Leo Sarry <sarry.leo@orange.fr>
2022-06-10 09:23:48 +0200

    tube3

--- src/metier/Tube.java
+++ src/metier/Tube.java
@@ -13,8 +13,39 @@ public class Tube
 		this.epaisseur = epaisseur;
 	}
 
-	public Tube creerTube(Cuve cv1, Cuve cv2, int epaisseur)
+	public static Tube creerTube(Cuve cv1, Cuve cv2, int epaisseur)
 	{
-		
+		if (epaisseur > 10 || epaisseur < 2)
+		{
+			return null;
+		}
+
+		if (cv1 == null || cv2 == null )
+		{
+			return null;
+		}
+		if (cv1 == cv2) 
+		{
+			return null;
+		}
+		return new Tube(cv1, cv2, epaisseur);
 	}
+
+	public int getEpaisseur ()
+	{
+		return this.epaisseur;
+	}
+
+	public Cuve getCuve1()
+	{
+		return this.cuve1;
+	}
+
+	public Cuve getCuve2()
+	{
+		return this.cuve2;
+	}
+
+	
+
 }
