0ad429dcfa0d10e45400109365a0c1f9192d9062
Leo Sarry <sarry.leo@orange.fr>
2022-06-10 09:06:03 +0200

    tube1

--- Tube.java
+++ Tube.java
@@ -2,4 +2,18 @@ public class Tube
 {
 	private Cuve cuve1;
 	private Cuve cuve2;	
+
+	private int epaisseur;
+
+	private Tube (Cuve cv1, Cuve cv2, int epaisseur)
+	{
+		this.cuve1     = cv1;
+		this.cuve2     = cv2;
+		this.epaisseur = epaisseur;
+	}
+
+	public Tube creerTube(Cuve cv1, Cuve cv2)
+	{
+		
+	}
 }
