476e7010b732c241d8355a983e46030695a0c444
Yamine Tamourt <yamine.tamourt@etu.univ-lehavre.fr>
2022-06-10 15:28:59 +0200

    Indentation

--- src/app2/metier/Reseau.java
+++ src/app2/metier/Reseau.java
@@ -20,20 +20,21 @@ public class Reseau
         this.lstTube = new ArrayList<Tube>();
     }
 
-    public boolean creerCuve(int capacite, int posX, int posY, String posInfo) {
+    public boolean creerCuve(int capacite, int posX, int posY, String posInfo)
+    {
         Cuve cuveACreer = Cuve.fabrique(capacite, posX, posY, posInfo);
 
         if (cuveACreer == null) {return false;}
 
-        for (Cuve c : this.lstCuve) {
+        for (Cuve c : this.lstCuve)
             if (c.getPosX() == posX && c.getPosY() == posY) {return false;}
-        }
 
         this.lstCuve.add(cuveACreer);
         return true;
     }
 
-    public boolean creerTube(Cuve cv1, Cuve cv2, int epaisseur) {
+    public boolean creerTube(Cuve cv1, Cuve cv2, int epaisseur)
+    {
         Tube tubeACreer;
         
         if (!this.lstCuve.contains(cv1) && !this.lstCuve.contains(cv2)) {return false;}
@@ -46,11 +47,7 @@ public class Reseau
         return true;
     }
 
-    public List<Tube> getTubes() {
-        return new ArrayList<Tube>(this.lstTube);
-    }
+    public List<Tube> getTubes() { return new ArrayList<Tube>(this.lstTube); }
 
-    public List<Cuve> getCuves() {
-        return new ArrayList<Cuve>(this.lstCuve);
-    }
+    public List<Cuve> getCuves() { return new ArrayList<Cuve>(this.lstCuve); }
 }
