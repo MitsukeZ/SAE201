e1eeb28003ddc31578b1d15f94ef2bcc4297e3b8
Leo Sarry <sarry.leo@orange.fr>
2022-06-10 13:04:19 +0200

    frame et panel

--- src/main/ihm/FrameFormulaire.java
+++ src/main/ihm/FrameFormulaire.java
@@ -2,13 +2,17 @@ package src.main.ihm;
 import java.awt.BorderLayout;
 
 import javax.swing.*;
+import javax.swing.plaf.ActionMapUIResource;
 
-public class FrameFormulaire extends JFrame
+import java.awt.event.*;
+
+public class FrameFormulaire extends JFrame implements ActionListener
 {
 	private Controleur        ctrl;
     private JPanel            panelValider;
     private PanelInformations panelInfos;
     private JButton           btnValider;
+    private int               compteur;
 	
     public FrameFormulaire (Controleur ctrl)
     {
@@ -20,9 +24,9 @@ public class FrameFormulaire extends JFrame
 
         // CrÃ©ation des composants
         this.panelValider = new JPanel();
-        this.panelInfos   = new PanelInformations(this.ctrl);
+        this.panelInfos   = new PanelInformations(this.ctrl, this);
         this.btnValider   = new JButton("Valider la saisie");
-
+        this.compteur = 0;
         // Positionnement des composants
         this.add(panelValider, BorderLayout.SOUTH);
         this.add(panelInfos, BorderLayout.CENTER);
@@ -31,10 +35,23 @@ public class FrameFormulaire extends JFrame
 
 
         //activation des composants
-
+        this.btnValider.addActionListener(this);
 
 
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }   
+
+    public void actionPerformed (ActionEvent e)
+    {
+        if (compteur == 0)
+        {
+
+        }
+    }
+
+    public int getCpt()
+    {
+        return this.compteur;
+    }
 }
--- src/main/ihm/PanelInformations.java
+++ src/main/ihm/PanelInformations.java
@@ -1,14 +1,48 @@
 package src.main.ihm;
 import javax.swing.*;
+import java.awt.GridLayout;
 
 public class PanelInformations extends JPanel
 {
-	private Controleur ctrl;
-    private JPanel     panelValider;
+	private Controleur      ctrl;
+	private FrameFormulaire frameMere;
+	private JPanel          panelGeneral;
+	private JTextField      txtNbElt;
+	private JTextField      txtCapacite;
 
-    public PanelInformations (Controleur ctrl)
-    {
-        this.ctrl = ctrl;
-    }
-	
+	public PanelInformations (Controleur ctrl, FrameFormulaire frame)
+	{
+		this.ctrl      = ctrl;
+		this.frameMere = frame;
+
+		// crÃ©ation des composants
+		this.txtNbElt = new JTextField(20);
+		this.panelGeneral = new JPanel();
+		this.panelGeneral.setLayout(new GridLayout(1,2) );
+		// Positionnement des composants
+
+		this.panelGeneral.add(new JLabel("Nombre de cuves :"), RIGHT_ALIGNMENT);
+		this.panelGeneral.add(this.txtNbElt);
+		this.add (this.panelGeneral);
+
+	}
+
+
+	public void passageEtapeSupp (int nrEtape)
+	{   
+		if ( nrEtape == 2 )
+		{
+			this.panelGeneral.removeAll();
+			this.txtCapacite = new JTextField(20);
+
+			this.panelGeneral.add(new JLabel("Capacité de la cuve"));
+			this.panelGeneral.add(this.txtCapacite);
+		}
+
+		if ( nrEtape == 3 )
+		{
+
+		}
+	}
+>
 }
