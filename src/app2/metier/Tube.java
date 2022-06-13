95b3b3b27bdf5a8514170251b55bdb300794f865
Leo Sarry <sarry.leo@orange.fr>
2022-06-13 09:58:41 +0200

    app2 tuve et cuve

--- src/app2/metier/Tube.java
+++ src/app2/metier/Tube.java
@@ -4,16 +4,16 @@ public class Tube
 	private Cuve cuve1;
 	private Cuve cuve2;	
 
-	private int epaisseur;
+	private double epaisseur;
 
-	private Tube (Cuve cv1, Cuve cv2, int epaisseur)
+	private Tube (Cuve cv1, Cuve cv2, double epaisseur)
 	{
 		this.cuve1     = cv1;
 		this.cuve2     = cv2;
 		this.epaisseur = epaisseur;
 	}
 
-	public static Tube creerTube(Cuve cv1, Cuve cv2, int epaisseur)
+	public static Tube creerTube(Cuve cv1, Cuve cv2, double epaisseur)
 	{
 		if (epaisseur > 10 || epaisseur < 2)
 		{
@@ -31,7 +31,7 @@ public class Tube
 		return new Tube(cv1, cv2, epaisseur);
 	}
 
-	public int getEpaisseur ()
+	public double getEpaisseur ()
 	{
 		return this.epaisseur;
 	}
@@ -47,5 +47,47 @@ public class Tube
 	}
 
 
-
+	public boolean transvaser()
+	{
+		Cuve grande;
+		Cuve petite;
+		grande = this.cuve1;
+		petite = this.cuve2;
+		if (this.cuve2.getContenu() > this.cuve1.getContenu())
+		{
+			grande = this.cuve2;
+			petite = this.cuve1;
+		}
+		if (grande.getContenu() != petite.getContenu())
+		{
+			if (petite.getQuantiteLibre() > this.epaisseur)
+			{
+				if (grande.getContenu() > this.epaisseur)
+				{
+					petite.remplir(this.epaisseur);
+					grande.vider  (this.epaisseur);
+				}
+				else
+				{
+					petite.remplir(grande.getContenu());
+					grande.vider  (grande.getContenu());
+				}
+			}
+			else
+			{
+				if (grande.getContenu() > petite.getQuantiteLibre())
+				{
+					grande.vider  (petite.getQuantiteLibre());
+					petite.remplir(petite.getQuantiteLibre());
+				}
+				else
+				{
+					petite.remplir(grande.getContenu());
+					grande.vider  (grande.getContenu());
+				}			
+			}
+			return true;
+		}
+		return false;
+	}
 }
