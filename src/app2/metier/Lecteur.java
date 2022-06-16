18646229ff51b5b34b84ffca644f0a61a6bfb059
Alexandra Desplan <alexandradesplan@gmail.com>
2022-06-16 14:34:25 +0200

    Ajout de l'attribut ctrl

--- src/app2/metier/Lecteur.java
+++ src/app2/metier/Lecteur.java
@@ -12,9 +12,13 @@ import javax.swing.JFileChooser;
 public class Lecteur
 {
 	private JFileChooser jfc;
+	private Controleur   ctrl;
 	
 	public Lecteur(Controleur ctrl, String cheminFichier)
 	{
+
+		this.ctrl = ctrl; 
+
 		try
 		{
 			Scanner sc = new Scanner ( new FileReader ( cheminFichier ) );
@@ -32,21 +36,21 @@ public class Lecteur
 					if (tabS[0].equals("liste d'adjacence") || tabS[0].equals("matrice de cout") || tabS[0].equals("matrice de cout optimisee"))
 						structure = tabS[0];
 					else
-						ctrl.creerCuve(Integer.parseInt(tabS[0]), Integer.parseInt(tabS[1]), Integer.parseInt(tabS[2]), tabS[3]);
+						this.ctrl.creerCuve(Integer.parseInt(tabS[0]), Integer.parseInt(tabS[1]), Integer.parseInt(tabS[2]), tabS[3]);
 				}
 
 				String[] tabS = sc.nextLine().split("\t");
 
 				if (structure.equals("liste d'adjacence"))
 				{
-					ctrl.creerTube(ctrl.getCuves().get(tabS[0].charAt(0) - 'A'), ctrl.getCuves().get(tabS[1].charAt(0) - 'A'), Double.parseDouble(tabS[2]) );
+					this.ctrl.creerTube(this.ctrl.getCuves().get(tabS[0].charAt(0) - 'A'), this.ctrl.getCuves().get(tabS[1].charAt(0) - 'A'), Double.parseDouble(tabS[2]) );
 				}
 				else
 				{
 					for (int i = 0; i < ligne; i++)
 					{
 						if (!tabS[i].equals("0"))
-							ctrl.creerTube(ctrl.getCuves().get(ligne), ctrl.getCuves().get(i), Double.parseDouble(tabS[i]));
+							this.ctrl.creerTube(this.ctrl.getCuves().get(ligne), this.ctrl.getCuves().get(i), Double.parseDouble(tabS[i]));
 					}
 				}
 
